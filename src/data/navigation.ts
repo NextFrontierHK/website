f981aa2b7ed9ae3009d7442a7ccefbc56ab2d42f
Yeison Rodriguez <yrodriguez@talenta.cloud>
2025-11-27 07:20:42 -0500

    styles: update styles

---
+++ src/data/navigation.ts
@@ -0,0 +1,14 @@
+export const NAVIGATION = {
+  menu: [
+    { label: 'Maas', href: '#projects' },
+    { label: 'Spects', href: '#freedoms' },
+    { label: 'Press', href: '#media' },
+    { label: 'Contact', href: '#contact' },
+  ],
+  sections: {
+    projects: 'Mission as a Service (MaaS)',
+    freedoms: 'Operational Freedoms',
+    media: 'Media Mentions',
+    contact: 'Let’s Connect'
+  }
+} as const;
